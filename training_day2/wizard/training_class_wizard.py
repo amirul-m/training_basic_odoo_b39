ff4181f99b8998ec30961021bc994b46061d6c55
Amirul <muh.amirul.muk@gmail.com>
2024-02-24 14:16:24 +0700

    [UPD] training_day2: add wizard [2]

--- training_day2/wizard/training_class_wizard.py
+++ training_day2/wizard/training_class_wizard.py
@@ -6,8 +6,13 @@ class TrainingClassWizard(models.TransientModel):
     _description = 'Training Class Wizard'
 
     name = fields.Char('Name', required=True)
+    max_person_wizard = fields.Integer('Max Peserta')
 
     def action_change(self):
         records = self.env[self._context.get('active_model')].browse(
             self._context.get('active_ids'))
-        records.write({'name': self.name})
+        new_values = {
+            'name': self.name,
+            'max_person': self.max_person_
+            }
+        records.write(new_values)
