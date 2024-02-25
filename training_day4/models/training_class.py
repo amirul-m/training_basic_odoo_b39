0c47c985b1f873bc2eb34a37b6ac209dd144f78d
Amirul <muh.amirul.muk@gmail.com>
2024-02-25 11:17:24 +0700

    [UPD] training_day2: inherit method confirm

--- training_day4/models/training_class.py
+++ training_day4/models/training_class.py
@@ -9,6 +9,17 @@ class TrainingClass(models.Model):
     description = fields.Text('First Description')
     class_type = fields.Selection(required=False)
 
+    def action_confirm(self):
+        # Validasi harus ada member
+        if not self.member_ids:
+            raise ValidationError('Members harus ada minimal 1')
+        
+        res = super().action_confirm()
+        
+        # Fill second description
+        self.second_description = 'Terkonfirmasi'
+        
+        return res
 
 class TrainingClass(models.Model):
     _name = 'training.class.copy'
