cab9c393289c8525ccb550a19059f23471635925
Amirul <muh.amirul.muk@gmail.com>
2024-02-25 14:51:42 +0700

    [UPD] training_day2: edit printout sales

--- training_day4/__manifest__.py
+++ training_day4/__manifest__.py
@@ -5,11 +5,12 @@
     'version' : '1.0',
     'category': 'Technical',
     'website': 'https://arkana.co.id',
-    'depends': ['training_day2', 'mail'],
+    'depends': ['training_day2', 'mail', 'sale'],
     'data': [
         "data/data.xml",
         "security/ir.model.access.csv",
         'views/training_class_view.xml',
+        'views/sale_order_report.xml',
     ],
     'license': 'LGPL-3',
 }
